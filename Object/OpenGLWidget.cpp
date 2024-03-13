8aecd007d4f9291d1e16e9f7fa511bbc104be84f
Luu <ludexian@126.com>
2024-03-13 21:12:12 +0800

    feat: 添加元素缓冲对象EBO

--- Object/OpenGLWidget.cpp
+++ Object/OpenGLWidget.cpp
@@ -33,5 +33,7 @@ void OpenGLWidget::paintGL()
 
 	d->m_shader->bind();
 	glBindVertexArray(d->m_vertexObj->m_VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);	// Ïß¿òÄ£Ê½
 }
--- Object/VertexObject.cpp
+++ Object/VertexObject.cpp
@@ -8,6 +8,7 @@ VertexObject::VertexObject()
 
 	glGenVertexArrays(1, &m_VAO);
 	glGenBuffers(1, &m_VBO);
+	glGenBuffers(1, &m_EBO);
 
 	float vertices[] = {
 		 0.5f,  0.5f, 0.0f,	// 右上角
@@ -15,10 +16,16 @@ VertexObject::VertexObject()
 		-0.5f, -0.5f, 0.0f,	// 左下角
 		-0.5f,  0.5f, 0.0f	// 左上角
 	};
-	
+	uint indices[] = {
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
+	};
 	glBindVertexArray(m_VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
